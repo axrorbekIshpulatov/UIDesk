9070fd49c628b9295b6f1249be82557ef17dc9e5
Saminabonu <mehrinisoxudayorova1@gmail.com>
2023-11-30 21:59:17 +0500

    added coment

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -3,3 +3,4 @@ export const Button = ({children, style}) => {
       <button className={`py-[16px] px-[40px] rounded-[50px] text-white text-[24px] font-normal bg-[#494949] ${style}`}>{children}</button>
     )
   }
+  {/* gradient-to-tr from-[#64BB47] to-[#2C9553] */}
